735cbec358f7def820eaa8a075f32838e0f169be
maheen arshad <maheen arshad>
2017-05-02 04:25:58 -0800

    splashscreen

--- app/src/main/java/com/example/maheen/projectsmd/MySQLiteHelper.java
+++ app/src/main/java/com/example/maheen/projectsmd/MySQLiteHelper.java
@@ -17,7 +17,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     // Database Version
     private static final int DATABASE_VERSION = 1;
     // Database Name
-    private static final String DATABASE_NAME = "HajjGuideFinaly.db";
+    private static final String DATABASE_NAME = "HajjGuideFiainaly.db";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
